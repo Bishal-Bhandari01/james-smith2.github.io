a6a80515d0f6df314310f319ef19e2f642e359c8
BISHAL BHANDARI <80243759+James-smith2@users.noreply.github.com>
2022-01-11 00:02:29 +0545

    main

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}else{
+			$('.navbar').removeClass("sticky");
+		}
+		if(this.scrollY > 500){
+			$('.scroll-up-btn').addClass("show");
+		}
+		else{
+			$('.scroll-up-btn').removeClass("show");
+		}
+	});
+	// slide up script
+	$('.scroll-up-btn').click(function(){
+		$('html').animate({scrollTop: 0});
+	});
+	// toggle menu/navbar script
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.menu-btn i').toggleClass("active");
+	});
+	// typing animation script
+	var typed = new Typed(".typing", {
+		strings: ["Youtuber.","Programmer.","Graphic Designer.","Gamer.","Twitch Streamer.","Freelancer."],
+		typeSpeed: 100,
+		fadeOut: true,
+		loop: true
+	});
+	var typed = new Typed(".typing-2", {
+		strings: ["I'm a Youtuber.","I'm a Twitch Streamer.","specializing in Graphic Designing.","a Freelancer.","I love to play video games.","I'm learning Cyber Security."],
+		typeSpeed: 100,
+		fadeOut: true,
+		loop: true
+	});
+
+	// play and pause section
+	var song = document.getElementById("sound");
+	var icons = document.getElementById("playicon");
+	icons.onclick = function(){
+		if(song.paused){
+			song.play();
+			icons.src = "./images/pause.png";
+		}else{
+			song.pause();
+			icons.src = "./images/play.png"
+		}
+	}
+});
